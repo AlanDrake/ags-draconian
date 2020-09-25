c08629288f9a108bd6d391e7e1cf68eda957a678
Alan v. Drake <alan.van.drake@gmail.com>
2020-09-26 01:50:38 +0200

    Script API: Game.SetDirectionRatio(float ratio) Allows to change the way the direction view is calculated. Can be useful for better matching the room perspective. What it does is change the importance of the X axis in the original calculations. Beware, this parameter is not currently serialized in savegames. Default is 1.0, from 2.0 to 3.0 could be good values for a room in perspective.

--- Engine/ac/character.cpp
+++ Engine/ac/character.cpp
@@ -76,6 +76,7 @@ extern int said_text;
 extern int our_eip;
 extern CCCharacter ccDynamicCharacter;
 extern CCInventory ccDynamicInv;
+extern float direction_ratio;
 
 //--------------------------------
 
@@ -321,6 +322,8 @@ enum DirectionalLoop
 DirectionalLoop GetDirectionalLoop(CharacterInfo *chinfo, int x_diff, int y_diff)
 {
     DirectionalLoop next_loop = kDirLoop_Left; // NOTE: default loop was Left for some reason
+
+    x_diff /= direction_ratio; // perspective adjustment by Game.SetDirectionRatio()
 
     const ViewStruct &chview  = views[chinfo->view];
     const bool has_down_loop  = ((chview.numLoops > kDirLoop_Down)  && (chview.loops[kDirLoop_Down].numFrames > 0));
