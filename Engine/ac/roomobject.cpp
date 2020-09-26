6fe8bebcdc134bde66b87e4f7bca3549c51daf63
Alan v. Drake <alan.van.drake@gmail.com>
2020-09-26 14:33:38 +0200

    Engine: pan audio for view sounds for objects and characters

--- Engine/ac/roomobject.cpp
+++ Engine/ac/roomobject.cpp
@@ -109,7 +109,7 @@ int RoomObject::GetFrameSoundVolume() const
 
 void RoomObject::CheckViewFrame()
 {
-    ::CheckViewFrame(view, loop, frame, GetFrameSoundVolume());
+    ::CheckViewFrame(view, loop, frame, GetFrameSoundVolume(), GetPanningFromPosition(x));
 }
 
 void RoomObject::ReadFromSavegame(Stream *in, int cmp_ver)
