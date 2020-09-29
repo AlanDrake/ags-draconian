2d573de3f1e7694501a36146e381fcd44df83b12
Alan v. Drake <alan.van.drake@gmail.com>
2020-09-29 03:48:03 +0200

    Fixed crash on reload with directional audio update_directional_sound_vol must be called after RestoreViewportsAndCameras since it depends on the viewport

--- Engine/game/savegame.cpp
+++ Engine/game/savegame.cpp
@@ -587,7 +587,6 @@ HSaveError DoAfterRestore(const PreservedParams &pp, RestoredData &r_data)
         }
     }
 
-    update_directional_sound_vol();
 
     adjust_fonts_for_render_mode(game.options[OPT_ANTIALIASFONTS] != 0);
 
@@ -597,6 +596,8 @@ HSaveError DoAfterRestore(const PreservedParams &pp, RestoredData &r_data)
     GUI::MarkAllGUIForUpdate(true, true);
 
     RestoreViewportsAndCameras(r_data);
+
+    update_directional_sound_vol(); // depends on viewport, so must be after RestoreViewportsAndCameras
 
     play.ClearIgnoreInput(); // don't keep ignored input after save restore
     update_polled_stuff();
