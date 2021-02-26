cfec669d7e194e63024aa4d826d3ec89ee6d749f
Alan v. Drake <alan.van.drake@gmail.com>
2021-02-26 11:35:24 +0100

    Script: Readable UseRegionTint property for Character/Object

--- Editor/AGS.Editor/Resources/agsdefns.sh
+++ Editor/AGS.Editor/Resources/agsdefns.sh
@@ -1859,6 +1859,8 @@ builtin managed struct Object {
   import attribute BlendMode BlendMode;
   /// Gets/sets the object's sprite rotation in degrees.
   import attribute float GraphicRotation;
+  /// Gets whether the object uses region tinting.
+  readonly import attribute bool UseRegionTint;
 #endif
   readonly int reserved[2];  // $AUTOCOMPLETEIGNORE$
 };
@@ -2088,6 +2090,8 @@ builtin managed struct Character {
   import attribute BlendMode BlendMode;
   /// Gets/sets the character's sprite rotation in degrees.
   import attribute float GraphicRotation;
+  /// Gets whether the character uses region tinting.
+  readonly import attribute bool UseRegionTint;
 #endif
   readonly int reserved_a[28];   // $AUTOCOMPLETEIGNORE$
   readonly short reserved_f[MAX_INV];  // $AUTOCOMPLETEIGNORE$
