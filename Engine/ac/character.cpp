AlanDrake/ags

Engine: pan audio for view sounds for objects and characters

Engine/ac/character.cpp
@@ -2132,6 +2132,7 @@ void animate_character(CharacterInfo *chap, int loopn,int sppd,int rept, int noi
 void CheckViewFrameForCharacter(CharacterInfo *chi) {
 
     int soundVolume = SCR_NO_VALUE;
+    int soundPanning = GetPanningFromPosition(chi->x);
 
     if (chi->flags & CHF_SCALEVOLUME) {
         // adjust the sound volume using the character's zoom level
@@ -2147,7 +2148,7 @@ void CheckViewFrameForCharacter(CharacterInfo *chi) {
             soundVolume = 100;
     }
 
-    CheckViewFrame(chi->view, chi->loop, chi->frame, soundVolume);
+    CheckViewFrame(chi->view, chi->loop, chi->frame, soundVolume, soundPanning);
 }
 
 Bitmap *GetCharacterImage(int charid, int *isFlipped) 

Engine/ac/roomobject.cpp
@@ -92,7 +92,8 @@ void RoomObject::UpdateCyclingView()
       return;
 
     wait=vfptr->speed+overall_speed;
-    CheckViewFrame (view, loop, frame);
+    int soundPanning = GetPanningFromPosition(x);
+    CheckViewFrame (view, loop, frame, SCR_NO_VALUE, soundPanning);
 }
 
 

AlanDrake/ags-draconian

Engine/ac/character.cpp
@@ -2187,7 +2187,8 @@ void CheckViewFrameForCharacter(CharacterInfo *chi)
         frame_vol = frame_vol * zoom_level / 100;
     }
 
-    CheckViewFrame(chi->view, chi->loop, chi->frame, frame_vol);
+    int soundPanning = GetPanningFromPosition(chi->x);
+    CheckViewFrame(chi->view, chi->loop, chi->frame, frame_vol, soundPanning);
 }
 
 Bitmap *GetCharacterImage(int charid, int *isFlipped) 

Engine/ac/roomobject.cpp
@@ -96,7 +96,8 @@ void RoomObject::UpdateCyclingView(int ref_id)
       return;
 
     wait=vfptr->speed+overall_speed;
-    CheckViewFrame(view, loop, frame, anim_volume);
+    int soundPanning = GetPanningFromPosition(x);
+    CheckViewFrame(view, loop, frame, anim_volume, soundPanning);
 }
 
 void RoomObject::ReadFromSavegame(Stream *in, int cmp_ver)